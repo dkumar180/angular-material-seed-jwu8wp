d9dd74970871a6e5e4c696c93e7b21cc0a159410
dkumar180 <deepukewlgrl@gmail.com>
2019-06-27 17:19:28 +0200

    1 file modified

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit  } from '@angular/core';
 
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
