64bb3fa5a23c690153691ea86478a2674713ac8a
dkumar180 <deepukewlgrl@gmail.com>
2019-06-27 18:28:13 +0200

    1 file modified

--- app/pages/about/about.component.ts
+++ app/pages/about/about.component.ts
@@ -8,26 +8,23 @@ export class AboutComponent {
 
   cards = [
     { 
-      title: 'About',
-      content: 'This is an Angular Seed on StackBlitz',
+      title: 'SOLL Matrix - Introduction',
+      content: 'The SOLL Matrix describes the authorisation and access implemented on the system. It is also called as the ING Access Manager.',
     },
     { 
-      title: 'Adding new pages',
+      title: 'Implementation',
       content: '',
       items: [
-        'Create your component: <code>pages/my-thing.component.ts</code>',
-        'Declare component in <code>pages/page.module.ts</code>',
-        'Add routes to component in <code>app-routing.module.ts</code>',
-        'Update navigation in <code>core/ui/ui.config.ts</code>',
+        'We are implementing a User Interface that makes it easy to add the Business roles and Technical Roles required for different platforms',
+        'Replacing the age old excel sheet format with an interactive UI that captures the required fields and also validates the entered information.',
+        'Auto populate information that can be retrieved from other systems like CMDB, Aveksa etc.',
       ]
     },
     { 
-      title: 'Dependencies',
+      title: 'UI Components',
       items: [
-        'Angular 4.x',
-        '@ngrx/store 4.x',
-        'Bootstrap 4 beta',
-        'Font Awesome 4.7',
+        'Cover Page: This contains all details related to the application, asset owner, CMDB registration etc. Approval from AO/DAO required before sending it to implementation team.',
+        'Subject Matrix - PA: This contains all the BR-TR mappings for the different platforms chosen on the first screen.',
       ]
     }
   ]
